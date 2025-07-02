b0d0c965c0ec0d20242101c412c5e4a855378b59
joshlhughes94 <joshlhughes94@gmail.com>
2025-07-02 19:23:38 +0100

    created framework and started adding tests

---
+++ BrightHRUITests/Services/PageService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrightHRUITests.Models;
+
+namespace BrightHRUITests.Services
+{
+        public interface IPageService
+        {
+            LoginPage LoginPage { get; }
+        }
+
+        public class PagesService : IPageService
+        {
+            public PagesService(LoginPage loginPage)
+            {
+                LoginPage = loginPage ?? throw new ArgumentNullException(nameof(loginPage)); 
+            }
+
+        public LoginPage LoginPage { get; }
+        }
+}
