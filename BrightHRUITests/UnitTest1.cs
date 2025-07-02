d78f8fdda0508bd1f66e710a6e1701facd3f6e14
joshlhughes94 <joshlhughes94@gmail.com>
2025-07-02 15:01:54 +0100

    Initial commit with Visual Studio project

---
+++ BrightHRUITests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BrightHRUITests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
