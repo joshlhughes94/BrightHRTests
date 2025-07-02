02afd015a4d1d1ea110420770250c842aca9c6a3
joshlhughes94 <joshlhughes94@gmail.com>
2025-07-02 15:09:22 +0100

    created framework

--- BrightHRUITests/obj/Debug/net8.0/BrightHRUITests.AssemblyInfo.cs
+++ BrightHRUITests/obj/Debug/net8.0/BrightHRUITests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrightHRUITests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5db5e66cec9773688456b5317f671b4237d0af92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78f8fdda0508bd1f66e710a6e1701facd3f6e14")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrightHRUITests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrightHRUITests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
