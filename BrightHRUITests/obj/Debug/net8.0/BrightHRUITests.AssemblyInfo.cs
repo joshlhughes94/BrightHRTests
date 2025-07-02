da18861782f1600b42e37feb15d0de77bff0e7a3
joshlhughes94 <joshlhughes94@gmail.com>
2025-07-02 19:36:51 +0100

    removing obj

--- BrightHRUITests/obj/Debug/net8.0/BrightHRUITests.AssemblyInfo.cs
+++ BrightHRUITests/obj/Debug/net8.0/BrightHRUITests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrightHRUITests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+003a21b168fc2a9bd8ead60f4109019bebccdf63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3249d3b912e23906b6be7bd397a9821cab3efd2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrightHRUITests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrightHRUITests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
